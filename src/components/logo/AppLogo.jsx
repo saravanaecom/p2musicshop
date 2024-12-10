2e8889902db2945aaa342683b95ea18783626661
saravanaecom <saravanaecomweb@gmail.com>
2024-12-10 11:16:15 +0530

    Add files via upload

--- src/components/logo/AppLogo.jsx
+++ src/components/logo/AppLogo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from './P2-Music-Shop.png';
 const AppLogo = ({ children }) => {
   return (
-    <img src={Logo} alt="logo" style={{width: '180px', height: '80px'}}/>
+    <img src={Logo} alt="logo" style={{width: '130px', height: '100px'}}/>
   );
 };
 
